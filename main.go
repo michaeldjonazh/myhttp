06f4dad3420956666806e84aaf611f02637e47cd
Jonazh Michael Davidy <jonazhm@fpt.com>
2024-09-06 11:33:37 +0900

    Initial seed

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/michaeldjonazh/myhttp/myhttp"
+)
+
+func main(){
+	myhttp.Hello()
+}
---
+++ myhttp/log.go
@@ -0,0 +1,11 @@
+package myhttp
+
+import (
+	"log"
+
+	_ "github.com/sirupsen/logrus"
+)
+
+func Hello(){
+	log.Println("Hello from My HTTP Package")
+}
